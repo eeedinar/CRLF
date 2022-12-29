46291d32907eb5ca25ac97676df9e283aadb79ba
Abdullah Al Bashit <39148573+eeedinar@users.noreply.github.com>
2022-12-28 22:39:58 -0500

    Add files via upload

---
+++ CRLF.py
@@ -0,0 +1,119 @@
+import pandas as pd
+import concurrent.futures
+import time
+import shutil
+import argparse
+from functools import partial
+from essential_func import *
+
+
+def compare_groud_truth(output_directory, folder):
+
+    ### Parameters and Analysis of model with the labels.csv file for CRLF only
+    gnd_file = 'labels.csv'
+    df_col_name_category = 'Category'
+    df_col_name_file = 'File'
+    df_col_name_call_start = 'Start'
+    frog_type = 'CRLF'
+    gen_comparison_file = 'comparison.csv'
+
+    ### read labels.csv, and results.csv files and write comparison.csv file
+    df2 = pd.read_csv(gnd_file)                     # read labels file
+    df2 = df2[df2[df_col_name_category]==frog_type]              # filter only CRLF only
+    df2.sort_values(by=[df_col_name_file], inplace=True)      # sort dataframe to be similar with df
+    n   = len(np.unique(df2[df_col_name_file].tolist()))      # get unique files 
+    X = np.zeros((n,2),dtype=object)
+
+    grp = df2.groupby(df_col_name_file)                       # group by file name only
+
+    for idx, ng in enumerate(grp):
+        name, group = ng
+        X[idx,0] = name
+        X[idx,1] = np.array(group[df_col_name_call_start])
+        
+    df = pd.read_csv(os.path.join(output_directory,folder,'results.csv'))         # read CRLF analyzed file    
+    df2 = pd.DataFrame(X)
+    df2.columns = [df_col_name_file, df_col_name_call_start]
+    df_outer = pd.merge(df, df2, on = df_col_name_file, how="outer")   # make sure all values are retained
+    df_outer.columns = ['File', 'Analyzed', 'Ground']
+    df_outer.to_csv(os.path.join(output_directory,folder,gen_comparison_file), index=False,)
+
+    print("Analysis is completed")
+
+def main(args):
+    print(f"Program started on {time.strftime('%m/%d/%Y %A %H:%M:%S', time.localtime())}")
+
+    #### Parameters - showing plot and results
+    show_plot    = False
+    show_results = False
+
+    ### create folder for data output
+    folder = f"{time.strftime('%Y-%m-%d-%A-%H-%M-%S', time.localtime())}"
+    os.makedirs(folder)
+
+    ### reading parameters and setting it to variables
+    file = open('parameters.txt','r')
+    file.seek(0)
+    parameters = file.readlines()
+    file.close()
+
+    for i in range(len(parameters)):
+        var, val = parameters[i].split("=")[0].strip() , parameters[i].split("=")[1].strip()
+        try:
+            globals()[var] = int(val)
+        except:
+            globals()[var] = val
+    ### look for wav and WAV extensions in the specified directory and produce results.csv file
+    files_sorted = cwd_files_search_with('.wav', directory = audio_directory)
+    files_sorted.append(cwd_files_search_with('.WAV', directory = audio_directory))
+    files_sorted = flatten(files_sorted)
+    n = len(files_sorted)
+    X = np.zeros((n,2),dtype=object)
+
+    ### Serial Processing - iterate over file and do signal detection
+    if args.mode == 'serial':
+        for idx, file in enumerate(files_sorted):
+            freq, t = signal_detection(filename = os.path.join(audio_directory, file), sr= sr, fmax=fmax, n_mels=n_mels, dB_thr=dB_thr, freq_idx=freq_idx, n_serials=n_serials, block=block, show_plot=show_plot, show_results=show_results, play_audio=play_audio )
+            X[idx,0] = file
+            X[idx,1] = t  
+
+    ### Multi-Processing - iterate over file and do signal detection
+    elif args.mode == 'parallel':
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            
+            arg = [os.path.join(audio_directory, file) for file in files_sorted]
+            worker = partial(signal_detection, sr= sr, fmax=fmax, n_mels=n_mels, dB_thr=dB_thr, freq_idx=freq_idx, n_serials=n_serials, block=block, show_plot=show_plot, show_results=show_results, play_audio=play_audio )
+            results = executor.map(worker, arg)
+
+            for idx, (file, t) in enumerate(zip(files_sorted, results)):
+                X[idx,0] = file
+                freq, X[idx,1] = t  
+
+    ### generates results.csv and parameters to the folder
+    df = pd.DataFrame(X)
+    df.columns = ['File', 'Start']
+    df.sort_values(by=['File'], inplace=True)
+    df.to_csv(os.path.join(output_directory,folder,'results.csv'), index=False)
+    shutil.copy2('parameters.txt', os.path.join(folder))     # copy parameters to the folder
+
+    ### log the outcome of this program
+    file = open(os.path.join(output_directory,folder,'output.log'), 'w')
+    file.write(f'Frequency used for computation is {freq:0.3f} Hz\nProgram was executed in {args.mode} mode')
+    file.close()
+
+    return output_directory, folder
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Arguments for CRLF detection program")
+    parser.add_argument('-m', '--mode',   type=str,   metavar='', required=False, default="parallel",  choices = ["serial", "parallel"],help = "serial or parallel operation on directory")
+    args = parser.parse_args()
+
+    output_directory, folder = main(args)
+    debug = True
+    if debug:
+        try:
+            compare_groud_truth(output_directory, folder)
+        except:
+            print("debug failed")
+    
+    print(f"Program completed on {time.strftime('%m/%d/%Y %A %H:%M:%S', time.localtime())}")
---
+++ essential_func.py
@@ -0,0 +1,70 @@
+import librosa
+import numpy as np
+import cv2 as cv
+import os
+from IPython.display import Audio
+import matplotlib.pyplot as plt
+import librosa.display
+
+
+### Misc functions
+def cwd_files_search_with(seek_str, search_where = 'end', directory = os.getcwd()):
+    """
+        files_sorted = cwd_files_search_with('.h5')
+    """
+    directory = os.getcwd() if directory == None else directory
+    files = []
+    if search_where == 'end':
+        for file in [each for each in os.listdir(directory) if each.endswith(seek_str)]:
+            files.append(file)
+    
+    elif search_where == 'start':
+        for file in [each for each in os.listdir(directory) if each.startswith(seek_str)]:
+            files.append(file)
+
+    files_sorted = sorted(files)
+    return files_sorted
+
+def flatten(S):
+    """
+    l = [2,[[1,2]],1]
+    list(flatten(l))
+    """
+    if S == []:
+        return S
+    if isinstance(S[0], list):
+        return flatten(S[0]) + flatten(S[1:])
+    return S[:1] + flatten(S[1:])
+
+
+def signal_detection(filename, sr, fmax, n_mels, dB_thr, freq_idx, n_serials, block, show_plot=False, show_results=False, play_audio = 0 ):
+    # f[freq_idx], t[r>n_serials] = signal_detection(filename= filename, sr= sr, fmax=fmax, n_mels=n_mels, dB_thr=dB_thr, freq_idx=freq_idx, n_serials=n_serials, block=block, show_plot=False, show_results=False, play_audio = 0 )
+    y, sr  = librosa.load(filename, sr = sr)
+    S      = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=n_mels, fmax=fmax, power=2.0,)
+    t      = np.linspace(start=0, stop=y.shape[0]/sr, num=S.shape[1])
+    f      = librosa.mel_frequencies(n_mels=n_mels, fmin=0.0, fmax=fmax, htk=False)
+    S_dB   = librosa.power_to_db(S, ref=np.max)
+    S_dB_thr = S_dB > dB_thr
+    kernel = np.ones((block,block),np.uint8)
+    S_closing = cv.morphologyEx(np.uint8(S_dB_thr), cv.MORPH_CLOSE, kernel)
+
+    if show_plot:
+        fig, axs = plt.subplots(ncols=3, nrows=1, sharex=True, sharey=True, figsize=(16,6))
+        img     = librosa.display.specshow(S_dB, x_axis='time', y_axis='mel', sr=sr, fmax=fmax, ax=axs[0])
+        img     = librosa.display.specshow(S_dB_thr, x_axis='time', y_axis='mel', sr=sr, fmax=fmax, ax=axs[1])
+        img     = librosa.display.specshow(S_closing, x_axis='time', y_axis='mel', sr=sr, fmax=fmax, ax=axs[2])
+        fig.colorbar(img, ax=ax, format='%+2.0f dB')
+        ax.set(title='Mel-frequency spectrogram')
+
+    S_block = S_closing[freq_idx,:]  # #np.array([ 0   ,  0  ,   0   ,  0  ,   0  ,   0    , 0  ,   0   ,  0  ,   1  ,   1  ,   1  ,   1   ,  1   ,  0   ,  0   ,  0  ,   0   ,  0  ,   0 ])
+    k = np.where( np.insert(np.diff(S_block)!=0, 0, True) )[0]  # insert true first an then find diff to find changes of indices --> array([ 0,  9, 14])
+    r = np.zeros_like(S_block)
+    tmp = k.tolist()
+    tmp.append(len(S_block))
+    r[k] = S_block[k]*np.array(np.diff(tmp))
+
+    if show_results:
+        print(f'frequency {f[freq_idx]}, and time {t[r>n_serials]}')
+    if play_audio:
+        Audio(data=y, rate=sr)
+    return f[freq_idx], t[r>n_serials]
